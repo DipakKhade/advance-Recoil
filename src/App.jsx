3e3b8f261fe82fa3551c3871b81948bc7c199a8b
= <=>
2024-02-27 09:49:39 +0530

    fetching and mapping data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useRecoilValue } from "recoil"
-import { dataAtom } from "./store/atoms/atoms"
+  import { useRecoilValue } from "recoil"
+  import { dataAtom } from "./store/atoms/atoms"
+import React from "react"
+
+  export default function App(){
 
 
-export default function App(){
-  const data=useRecoilValue(dataAtom)
-  console.log(data)
-  return(
-    <>
-      
-    </>
-  )
-}
+    return(
+      <>
+      hii
+      <GetData/>
+     
+      </>
+    )
+  }
+
+
+   function GetData(){
+    const data= useRecoilValue(dataAtom)
+    const maindata= data.data
+    console.log(maindata)
+
+    return(
+      <>
+{JSON.stringify(maindata)}
+      {
+        maindata.map(function(index,data){
+        <div key={index} className="border rounded p-4 mb-4">
+        <h2 className="text-lg font-bold">{data.title}</h2>
+        <p className="mt-2">{data.completed ? 'Completed' : 'Not Completed'}</p>
+      </div>
+        })
+      }
+      </>
+    )
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RecoilRoot } from 'recoil'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
   <RecoilRoot>
 
+<React.Suspense fallback={<div>Loading...</div>}>
     <App />
+    </React.Suspense>
   </RecoilRoot>
-  </React.StrictMode>,
 )
